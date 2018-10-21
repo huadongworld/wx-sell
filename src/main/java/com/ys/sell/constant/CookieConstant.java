a19048aadea2874d1aabf78f3294748b8cd363b5
huadong <hd2260332898@gmail.com>
2018-10-21 16:45:01 +0800

    卖家扫码登录，登出

---
+++ src/main/java/com/ys/sell/constant/CookieConstant.java
@@ -0,0 +1,12 @@
+package com.ys.sell.constant;
+
+/**
+ * @author HD
+ * @date 2018/10/21 16:04
+ */
+public interface CookieConstant {
+
+    String TOKEN = "token";
+
+    Integer EXPIRE = 7200;
+}
---
+++ src/main/java/com/ys/sell/constant/RedisConstant.java
@@ -0,0 +1,15 @@
+package com.ys.sell.constant;
+
+/**
+ * @author HD
+ * @date 2018/10/21 15:54
+ */
+public interface RedisConstant {
+
+    String TOKEN_PREFIX = "token_%s";
+
+    /**
+     * 2小时
+     */
+    Integer EXPIRE = 7200;
+}
