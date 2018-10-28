4137ac2241313a34d865f2ae9dbf3c612f88d67c
huadong <hd2260332898@gmail.com>
2018-10-28 20:20:29 +0800

    异常的捕获

--- src/main/java/com/ys/sell/handler/SellExceptionHandler.java
+++ src/main/java/com/ys/sell/handler/SellExceptionHandler.java
@@ -1,10 +1,16 @@
 package com.ys.sell.handler;
 
+import com.ys.sell.common.ResultObject;
 import com.ys.sell.config.ProjectUrlConfig;
+import com.ys.sell.exception.SellException;
 import com.ys.sell.exception.SellerAuthorizeException;
+import com.ys.sell.utils.ResultObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -20,7 +26,7 @@ public class SellExceptionHandler {
     /**
      * 拦截登录异常(SellerAuthorizeAspect中会抛出SellerAuthorizeException异常，抛出时使用这个modelAndView
      *
-     * http://huadong.natapp4.cc/sell/wechat/qrAuthorize?returnUrl=http://huadong.natapp4.cc/sell/seller/login
+     * http://huadong.natapp1.cc/sell/wechat/qrAuthorize?returnUrl=http://huadong.natapp1.cc/sell/seller/login
      */
     @ExceptionHandler(value = SellerAuthorizeException.class)
     public ModelAndView handlerAuthorizeException() {
@@ -30,5 +36,12 @@ public class SellExceptionHandler {
         .concat("?returnUrl=")
         .concat(projectUrlConfig.getSell())
         .concat("/sell/seller/login"));
+    }
+
+    @ExceptionHandler(value = SellException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.FORBIDDEN) //返回的调用状态
+    public ResultObject handlerSellException(SellException e) {
+        return ResultObjectUtils.error(e.getCode(), e.getMessage());
     }
 }
